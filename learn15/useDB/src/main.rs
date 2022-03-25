2cb7774af60b43f37b34871912c863e76db05474
IshigamiRyoichi <s19t301@stu.kagawa-u.ac.jp>
2022-03-24 23:38:55 -0700

    データベースの参照

--- learn15/useDB/src/main.rs
+++ learn15/useDB/src/main.rs
@@ -109,7 +109,16 @@ fn test6() -> Result<()>{
     let args = std::env::args().collect::<Vec<String>>();
     let cn = Connection::open("sample.db")?;
     if args.len() <= 1{
-        println!("not set parameter");
+        let mut stmt = cn.prepare("select * from users")?;
+        for it in stmt.query_map(params![], |row|{
+            Ok(User{
+                id: row.get(0)?,
+                name: row.get(1)?,
+                age: row.get(2)?,
+            })
+        })?{
+            println!("{:?}",it.unwrap());
+        }
     } else {
         match args[1].as_str() {
             "init" => {
@@ -118,7 +127,7 @@ fn test6() -> Result<()>{
             },
             "into" => {
                 let id: i32 = args[2].parse::<i32>().unwrap();
-                let name: &args[3];
+                let name = &args[3];
                 let age:i32 = args[4].parse::<i32>().unwrap();
                 let mut stmt = cn.prepare("insert into users (id, name, age) values(?,?,?)")?;
                 stmt.execute(params![id,name,age])?;
